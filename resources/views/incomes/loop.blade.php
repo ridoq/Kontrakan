ff2cf0379da2c9e5b582ec88aceda177631727d7
ridoq <ridoq444@gmail.com>
2024-08-01 11:15:45 +0700

    ppp

--- resources/views/incomes/loop.blade.php
+++ resources/views/incomes/loop.blade.php
@@ -1,14 +1,14 @@
 @section('acc')
     <td>
         <div class="row">
-            <form action="{{ route('incomes.accept', $income->id) }}" method="POST" class="col-lg-6 col-sm-12 mt-1" >
+            <form action="{{ route('incomes.accept', $income->id) }}" method="POST" class="col-lg-6 col-sm-6 mt-1">
                 @csrf
 
-                <button type="submit" class="col-12 btn btn-label-primary" type="button">Verify</button>
+                <button type="submit" class="col-12 btn btn-success" type="button"><i class="ri-check-line"></i></button>
             </form>
-            <form action="{{ route('incomes.reject', $income->id) }}" method="POST" class="col-lg-6 col-sm-12 mt-1">
+            <form action="{{ route('incomes.reject', $income->id) }}" method="POST" class="col-lg-6 col-sm-6 mt-1">
                 @csrf
-                <button type="submit" class="col-12 btn btn-label-danger" type="button">Rejects</button>
+                <button type="submit" class="col-12 btn btn-danger" type="button"><i class="ri-close-line"></i></button>
             </form>
         </div>
     </td>
@@ -19,8 +19,22 @@
             <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
                     class="ri-more-line"></i></button>
             <div class="dropdown-menu">
-                <a class="dropdown-item" href="javascript:void(0);"><i class="ri-pencil-line me-2"></i> Edit</a>
                 <a class="dropdown-item" href="javascript:void(0);"><i class="ri-delete-bin-7-line me-2"></i> Delete</a>
+            </div>
+        </div>
+    </td>
+@endsection
+@section('btnCancel')
+    <td>
+        <div class="dropdown">
+            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
+                    class="ri-more-2-line"></i></button>
+            <div class="dropdown-menu">
+                <form action="{{ route('incomes.cancel', $income->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="dropdown-item"><i class="ri-close-line me-2"></i>Cancel</button>
+                </form>
             </div>
         </div>
     </td>
